9afb15f6115d85ebaecb36161911d8a8d450d040
Balint Kleri <b@balintbrews.com>
2026-05-12 23:38:56 +0200

    feat(recipes:humanify): redesign components

---
+++ recipes/canvas_dev_humanify/content/file/types-BjfHlxnY.js
@@ -0,0 +1,1 @@
+import{isValidElement as p}from"react";const o=e=>p(e)&&typeof e.props.value=="string"&&e.props.value.includes("canvas--slot-empty-placeholder");export{o as hasEmptySlotPlaceholder};
